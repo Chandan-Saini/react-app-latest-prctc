0066f5dd2aa3848207d1fde02cb9dc024ed15513
Chandan Kumar <descentchandan@gmail.com>
2020-08-09 21:35:19 +0530

    my first change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>this is my first change live </h1>
     </div>
   );
 }
