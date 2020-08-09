7fcf425e074b17200865a5228704f7e21c41262e
Chandan Kumar <descentchandan@gmail.com>
2020-08-10 00:31:55 +0530

    used createElement function insted of JSX

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h2>hello</h2>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
