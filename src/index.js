736e4ef031a2a51357b03cf28e662c762d6891a8
Chandan Kumar <descentchandan@gmail.com>
2020-08-09 22:56:03 +0530

    used jsx insted of component during render

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h2>hello</h2>
   </React.StrictMode>,
   document.getElementById('root')
 );
