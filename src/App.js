f3cf572040c13d8f7a840d49586dc1c0eb7678d8
Chandan Kumar <descentchandan@gmail.com>
2020-08-11 00:53:35 +0530

    returned to class component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import './App.css';
 import Person from './Person/Person';
 
-const App= (props)=> {
+class App extends Component {
  
+  state={
+    person:[
+    {name:"Aman",age:20},
+    {name:"Akku",age:24},
+    {name:"chan",age:22}
+  ],
+  otherState:'some other state'
+}
 
-const [personState, setPersonState] = useState({
-  person: [
-    { name: "Aman", age: 20 },
-    { name: "Akku", age: 24 },
-    { name: "chan", age: 22 }
-  ]
-})
-
-const [otherState,setOtherState]=useState("this is other state")
-
- const eventHandler =()=>{
-   setPersonState({
+eventHandler=()=>{
+   this.setState({
      person: [
-       { name: "Aman updated again", age: 20 },
+       { name: "Aman updated", age: 20 },
        { name: "Akku", age: 24 },
        { name: "chan", age: 90 }
      ]
    })
 }
-   
-  console.log(personState,otherState);
+  render(){
     return (
       <div className="App">
         <h1>this is my first react app </h1>
-        <button onClick={eventHandler}>Switch</button>
-        <Person name={personState.person[0].name} age={personState.person[0].age} />
-        <Person name={personState.person[1].name} age={personState.person[1].age}> this is a text</Person>
-        <Person name={personState.person[2].name} age={personState.person[2].age} />
+        <button onClick={this.eventHandler}>Switch</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age} />
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}> this is a text</Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age} />
       </div>
 
     );
+  }
 
   // return React.createElement("div",{className:"App"},React.createElement("h1",null,"Hi this is my first change"))
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from "react"
 const person=(props)=>{
     return (
   <div>
-    <h3> My name is {props.name} and age is {props.age}</h3>
+    <h3 onClick={props.click}> My name is {props.name} and age is {props.age}</h3>
     <p>{props.children}</p> 
   </div>    )
 }
