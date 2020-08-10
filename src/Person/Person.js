2a441671631914b1d128236802a3c34fdcab9ef0
Chandan Kumar <descentchandan@gmail.com>
2020-08-10 15:31:22 +0530

    used {} inside comp to use JS code

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from "react"
 
 let person=()=>{
     return (
-        <h3> This is from new person component</h3>
+        <h3> This is from new {Math.random()*10} person component</h3>
     )
 }
 
