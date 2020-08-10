01789995491aec4fed0c23f0eaad5c340665725c
Chandan Kumar <descentchandan@gmail.com>
2020-08-10 15:20:04 +0530

    created and imported new comp from Person

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import Person from './Person/Person';
 function App() {
   return (
     <div className="App">
        <h1>this is my first change live </h1>
+      <Person />
     </div>
+   
   );
 
   // return React.createElement("div",{className:"App"},React.createElement("h1",null,"Hi this is my first change"))
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+let person=()=>{
+    return (
+        <h3> This is from new person component</h3>
+    )
+}
+
+export default person
