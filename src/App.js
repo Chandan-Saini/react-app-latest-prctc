5c991703ad978bf7fd0c4258c21788ae5171a063
Chandan Kumar <descentchandan@gmail.com>
2020-08-10 15:09:17 +0530

    made Person file and folder

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //      <h1>this is my first change live </h1>
-  //   </div>
-  // );
+  return (
+    <div className="App">
+       <h1>this is my first change live </h1>
+    </div>
+  );
 
-  return React.createElement("div",{className:"App"},React.createElement("h1",null,"Hi this is my first change"))
+  // return React.createElement("div",{className:"App"},React.createElement("h1",null,"Hi this is my first change"))
 }
 
 export default App;
