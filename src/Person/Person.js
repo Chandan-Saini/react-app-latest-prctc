d7c85eca49e1d1a28aa5f3b7022a1261370cae8d
Chandan Kumar <descentchandan@gmail.com>
2020-08-11 16:27:19 +0530

    styled Person.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from "react"
-
+import "./Person.css"
 const person=(props)=>{
     return (
-  <div>
+  <div className="Person">
     <h3 onClick={props.click}> My name is {props.name} and age is {props.age}</h3>
     <p>{props.children}</p>
     <input type="text" onChange={props.change} value={props.name} /> {/* this is the usage of two way binding*/}
