c06bfe6dd76f4ee8b0f01d4719e0947112b3a5fa
Chandan Kumar <descentchandan@gmail.com>
2020-08-11 15:46:52 +0530

    adding two way binding

--- src/App.js
+++ src/App.js
@@ -22,13 +22,24 @@ eventHandler=(newName)=>{
      ]
    })
 }
+
+  changeHandler = (event) => {
+    this.setState({
+      person: [
+        { name: "Aman", age: 20 },
+        { name: event.target.value, age: 24 },
+        { name: "chan", age: 90 }
+      ]
+    })
+  }
+
   render(){
     return (
       <div className="App">
         <h1>this is my first react app </h1>
-        <button onClick={()=> this.eventHandler("updated")}>Switch</button>
+        <button onClick={this.eventHandler.bind(this, "Updated")}>Switch</button>  {/* this is the way of passing value in the event handler using bind */}
         <Person name={this.state.person[0].name} age={this.state.person[0].age} />
-        <Person click={this.eventHandler.bind(this, "Updated 2")}  name={this.state.person[1].name} age={this.state.person[1].age}> this is a text</Person>
+        <Person change={this.changeHandler} click={this.eventHandler.bind(this, "Updated 2")}  name={this.state.person[1].name} age={this.state.person[1].age}> this is a text</Person>
         <Person name={this.state.person[2].name} age={this.state.person[2].age} />
       </div>
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,8 @@ const person=(props)=>{
     return (
   <div>
     <h3 onClick={props.click}> My name is {props.name} and age is {props.age}</h3>
-    <p>{props.children}</p> 
+    <p>{props.children}</p>
+    <input type="text" onChange={props.change} value={props.name} /> {/* this is the usage of two way binding*/}
   </div>    )
 }
 
