1cea4588304097cfc44eaba1f0a6afd5d5df41a5
Chandan Kumar <descentchandan@gmail.com>
2020-08-12 17:05:07 +0530

    integrating the persons name with input

--- src/App.js
+++ src/App.js
@@ -16,13 +16,16 @@ class App extends Component {
 
 
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event,id) => {
+    const personIndex= this.state.persons.findIndex(p=>{ return p.id===id})
+       const person= {...this.state.persons[personIndex]}
+
+       person.name=event.target.value
+      
+      const persons=[...this.state.persons]
+       persons[personIndex]=person
     this.setState({
-      persons: [
-        { name: "Aman", age: 20 },
-        { name: event.target.value, age: 24 },
-        { name: "chan", age: 90 }
-      ]
+      persons:persons
     })
   }
 
@@ -53,7 +56,7 @@ class App extends Component {
      if (this.state.showPersons) {
        persons= (
          this.state.persons.map((person,index)=>{
-           return <Person click={()=>this.deletePerson(index)} name={person.name} age={person.age} key={person.id} />
+           return <Person change={(event)=>this.nameChangedHandler(event, person.id)} click={()=>this.deletePerson(index)} name={person.name} age={person.age} key={person.id} />
          })
        )
      }
