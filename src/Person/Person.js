80f4c037208fb253c1f2fee74c6e302b886742d7
Chandan Kumar <descentchandan@gmail.com>
2020-08-13 22:22:35 +0530

    implemented css modules succesfully

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from "react"
-import "./Person.css"
+import styles from "./Person.module.css"
 
 const person=(props)=>{
   
   return (
-  <div className="Person" >
+  <div className={styles.Person} >
     <h3 onClick={props.click}> My name is {props.name} and age is {props.age}</h3>
     <p>{props.children}</p>
     <input type="text" onChange={props.change} value={props.name} /> {/* this is the usage of two way binding*/}
