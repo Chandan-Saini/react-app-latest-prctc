fd993a5020f0b359e988ab07344a4798bd1f415a
Chandan Kumar <descentchandan@gmail.com>
2020-08-13 17:28:27 +0530

    used media query and styleroot

--- src/App.js
+++ src/App.js
@@ -1,97 +1,105 @@
 import React, { Component } from "react";
-import './App.css';
-import Person from './Person/Person';
-import Radium from "radium"
+import "./App.css";
+import Person from "./Person/Person";
+import Radium, { StyleRoot } from "radium";
 
 class App extends Component {
- 
-  state={
-    persons:[
-    {id:"jygy1",name:"Aman",age:20},
-    {id:"jygy2",name:"Akku",age:24},
-    {id:"jygy3",name:"chan",age:22}
-  ],
-  otherState:'some other state',
-  showPersons:false
-}
+  state = {
+    persons: [
+      { id: "jygy1", name: "Aman", age: 20 },
+      { id: "jygy2", name: "Akku", age: 24 },
+      { id: "jygy3", name: "chan", age: 22 },
+    ],
+    otherState: "some other state",
+    showPersons: false,
+  };
 
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+    const person = { ...this.state.persons[personIndex] };
 
+    person.name = event.target.value;
 
-  nameChangedHandler = (event,id) => {
-    const personIndex= this.state.persons.findIndex(p=>{ return p.id===id})
-       const person= {...this.state.persons[personIndex]}
-
-       person.name=event.target.value
-      
-      const persons=[...this.state.persons]
-       persons[personIndex]=person
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
     this.setState({
-      persons:persons
-    })
-  }
+      persons: persons,
+    });
+  };
 
-  togglePersonsHandler=()=>{
-     const doesShow=  this.state.showPersons
-     this.setState({
-       showPersons: !doesShow
-     })
-  }
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow,
+    });
+  };
 
-  deletePerson=(index)=>{
+  deletePerson = (index) => {
     // const persons= this.state.persons.slice() //this slice here is returning a new array so that we do not directly manuplate the original array, instead of slice we can also use spread operator "..."
-    const persons= [...this.state.persons]
-    persons.splice(index,1)
-    this.setState({persons:persons})
-  }
+    const persons = [...this.state.persons];
+    persons.splice(index, 1);
+    this.setState({ persons: persons });
+  };
 
-  render(){
+  render() {
+    const style = {
+      backgroundColor: "green",
+      border: "2px solid yellow",
+      padding: "8px",
+      color: "white",
+      outline: "none",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black",
+      },
+    };
 
-   const style={
-     backgroundColor:"green",
-     border:"2px solid yellow",
-     padding:"8px",
-     color:"white",
-     outline:"none",
-     ':hover':{
-       backgroundColor:"lightgreen",
-       color:"black"
-     }
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = this.state.persons.map((person, index) => {
+        return (
+          <Person
+            change={(event) => this.nameChangedHandler(event, person.id)}
+            click={() => this.deletePerson(index)}
+            name={person.name}
+            age={person.age}
+            key={person.id}
+          />
+        );
+      });
+      style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black",
+      };
     }
 
-    let persons=null
-
-     if (this.state.showPersons) {
-       persons= (
-         this.state.persons.map((person,index)=>{
-           return <Person change={(event)=>this.nameChangedHandler(event, person.id)} click={()=>this.deletePerson(index)} name={person.name} age={person.age} key={person.id} />
-         })
-       )
-       style.backgroundColor="red"
-       style[":hover"]={
-         backgroundColor:"salmon",
-         color:"black"
-       }
-     }
-     
-     const classes=[]
-     if (this.state.persons.length <=2) {
-       classes.push("red")
-     }
-     if (this.state.persons.length <=1) {
-       classes.push("bold")
-     }
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push("red");
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
+    }
 
     return (
-      <div  className="App">
-        <h1 className={classes.join(" ")}>this is my first react app </h1>
-        <button style={style} onClick={this.togglePersonsHandler}>Show Names</button>  {/* this is the way of passing value in the event handler using bind */}
-       {persons}
-      </div>
-        )
-  
+      <StyleRoot>
+        <div className="App">
+          <h1 className={classes.join(" ")}>this is my first react app </h1>
+          <button style={style} onClick={this.togglePersonsHandler}>
+            Show Names
+          </button>{" "}
+          {/* this is the way of passing value in the event handler using bind */}
+          {persons}
+        </div>
+      </StyleRoot>
+    );
 
-  // return React.createElement("div",{className:"App"},React.createElement("h1",null,"Hi this is my first change"))
-    }
+    // return React.createElement("div",{className:"App"},React.createElement("h1",null,"Hi this is my first change"))
   }
+}
 
 export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,18 @@
 import React from "react"
 import "./Person.css"
+import Radium from "radium";
 const person=(props)=>{
-    return (
-  <div className="Person">
+  const style={
+  '@media (min-width:700px)':{
+     width:"600px"
+  }
+  }  
+  return (
+  <div className="Person" style={style}>
     <h3 onClick={props.click}> My name is {props.name} and age is {props.age}</h3>
     <p>{props.children}</p>
     <input type="text" onChange={props.change} value={props.name} /> {/* this is the usage of two way binding*/}
   </div>    )
 }
 
-export default person
+export default Radium(person)
