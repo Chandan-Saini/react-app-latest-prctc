f5c75d2d9c5987ab397030d6f74051aa144e1f78
Chandan Kumar <descentchandan@gmail.com>
2020-08-15 23:58:15 +0530

    chng try 8

--- src/containers/App.js
+++ src/containers/App.js
@@ -57,7 +57,7 @@ class App extends Component {
     return (
      
         <div className={classes.App}>
-         <Cockpit title={this.props.app} showPersons={this.state.showPersons} persons={this.state.persons} togglePersonsHandler={this.togglePersonsHandler} />
+         <Cockpit title={this.props.appTitle} showPersons={this.state.showPersons} persons={this.state.persons} togglePersonsHandler={this.togglePersonsHandler} />
           {persons}
         </div>
     );
