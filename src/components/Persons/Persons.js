fdd5b6f013396c15c8adc674b8a75b9443580bd5
Chandan Kumar <descentchandan@gmail.com>
2020-08-16 15:46:48 +0530

    changes persons.js to work with update life cycle

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,15 +1,19 @@
-import React from "react"
+import React, { Component } from "react"
 
 import Person from "./Person/Person"
 
-const persons = (props) => props.persons.map((person, index) => {  
-    return <Person
-        change={(event) => props.nameChangedHandler(event, person.id)}
-        click={() => props.deletePerson(index)}
-        name={person.name}
-        age={person.age}
-        key={person.id}
-    />
-}) 
-
-export default persons
+class Persons extends Component{
+    render(){
+        console.log("Persons render met");
+        return this.props.persons.map((person, index) => {
+            return <Person
+                change={(event) => this.props.nameChangedHandler(event, person.id)}
+                click={() => this.props.deletePerson(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+            />
+        }) 
+    } 
+}
+export default Persons
