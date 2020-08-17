3af58db20ced816bd21df797d7cc938df8eb7903
Chandan Kumar <descentchandan@gmail.com>
2020-08-17 14:34:40 +0530

    way 2 to pass multiple ele using aux.js and hoc

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,18 @@
 import React,{Component} from "react"
 import styles from "./Person.module.css"
+import Aux from "../../../hoc/Auxiliary"
 
 class Person extends Component{
   
   render(){
     console.log("Person.js render met");
-    return([ <h3 onClick={this.props.click}> My name is {this.props.name} and age is {this.props.age}</h3>,
-        <p>{this.props.children}</p>,
-        <input type="text" onChange={this.props.change} value={this.props.name} />
-    ])
+    return( <Aux>
+      <h3 onClick={this.props.click}> My name is {this.props.name} and age is {this.props.age}</h3>
+      <p>{this.props.children}</p>
+      <input type="text" onChange={this.props.change} value={this.props.name} />
+    </Aux>
+   
+    )
 
     }
 }
