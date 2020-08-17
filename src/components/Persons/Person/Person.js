2183d816c72747ff6fd0367d5d55c09d6ed5cd7d
Chandan Kumar <descentchandan@gmail.com>
2020-08-17 17:51:19 +0530

    way1 to use ref to make last input focus

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,13 +5,15 @@ import withClass from "../../../hoc/withClass"
 import PropTypes from "prop-types"
 import Persons from "../Persons"
 class Person extends Component{
-  
+  componentDidMount(){
+    this.InputElRef.focus()
+  }
   render(){
     console.log("Person.js render met");
     return( <Aux>
       <h3 onClick={this.props.click}> My name is {this.props.name} and age is {this.props.age}</h3>
       <p>{this.props.children}</p>
-      <input type="text" onChange={this.props.change} value={this.props.name} />
+      <input ref={(inpEl)=> this.InputElRef=inpEl} type="text" onChange={this.props.change} value={this.props.name} />
     </Aux>
   )  }
 }
