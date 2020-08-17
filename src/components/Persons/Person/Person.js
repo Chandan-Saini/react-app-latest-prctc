3daa9387eb0c4c05ef8eec8432d23a8d8d6dfa25
Chandan Kumar <descentchandan@gmail.com>
2020-08-17 14:20:00 +0530

    way 1 of using adjacent jsx elements using array

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,12 +5,10 @@ class Person extends Component{
   
   render(){
     console.log("Person.js render met");
-    return(
-      <div className = { styles.Person } >
-        <h3 onClick={this.props.click}> My name is {this.props.name} and age is {this.props.age}</h3>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.change} value={this.props.name} /> {/* this is the usage of two way binding*/ }
-  </div>    )
+    return([ <h3 onClick={this.props.click}> My name is {this.props.name} and age is {this.props.age}</h3>,
+        <p>{this.props.children}</p>,
+        <input type="text" onChange={this.props.change} value={this.props.name} />
+    ])
 
     }
 }
