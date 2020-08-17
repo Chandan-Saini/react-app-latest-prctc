c2bca975ac4852e0dd5cf774059778c1a0717f5e
Chandan Kumar <descentchandan@gmail.com>
2020-08-17 14:45:56 +0530

    back to hoc and aux.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React,{Component,Fragment} from "react"
+import React,{Component} from "react"
 import styles from "./Person.module.css"
 import Aux from "../../../hoc/Auxiliary"
 
@@ -6,11 +6,11 @@ class Person extends Component{
   
   render(){
     console.log("Person.js render met");
-    return( <Fragment>
+    return( <Aux>
       <h3 onClick={this.props.click}> My name is {this.props.name} and age is {this.props.age}</h3>
       <p>{this.props.children}</p>
       <input type="text" onChange={this.props.change} value={this.props.name} />
-    </Fragment>
+    </Aux>
    
     )
 
